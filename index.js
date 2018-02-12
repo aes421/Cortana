5a1249c526dc4a25fcbcb107edb638ea5fab3c9e
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-02-12 17:54:06 -0600

    interfacing bot builder with pokeapi

---
+++ index.js
@@ -0,0 +1,29 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var request = require('request');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    var endpoint = 'https://pokeapi.co/api/v2/pokemon/' + session.message.text;
+    request(endpoint, function(err, resp, body){
+        if(!err && resp.statusCode == 200){
+			session.send(JSON.parse(body).name);
+        }
+    });
+
+});
