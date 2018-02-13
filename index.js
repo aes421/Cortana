1a344c3c379b39e23c1d2e728f8cc37a41eb2a84
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-02-12 22:29:38 -0600

    setup for heroku deployment

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+    appId: process.env.MicrosoftAppId, //"5543f605-7b2b-457a-8a54-4483c67348ed"
+    appPassword: process.env.MicrosoftAppPassword //"uquNT447]luvvCYAUT65![)"
 });
 
 // Listen for messages from users
