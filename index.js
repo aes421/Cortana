e2a1d220d61fbbb35716ccdf8ea5434c507a2946
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-02-12 19:42:17 -0600

    uses waterfall to manage conversation in multiple dialogs to get a stat for a specified pokemon using poke api

--- index.js
+++ index.js
@@ -17,13 +17,57 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
+var inMemoryStorage = new builder.MemoryBotStorage();
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    var endpoint = 'https://pokeapi.co/api/v2/pokemon/' + session.message.text;
-    request(endpoint, function(err, resp, body){
-        if(!err && resp.statusCode == 200){
-			session.send(JSON.parse(body).name);
-        }
-    });
+var bot = new builder.UniversalBot(connector, [
+    //waterfall to control conversation
+    function (session) {
+        session.send("Welcome to Poke A.P.I.");
+        session.beginDialog('askForPokemon');
+    },
 
-});
+    function (session, results) {
+        session.dialogData.pokemon = results.response;
+        session.beginDialog('askForStat');
+    },
+
+    function (session, results) {
+        session.dialogData.stat = results.response;
+        //for use in callback
+        var data = session.dialogData;
+        //do work
+        var endpoint = 'https://pokeapi.co/api/v2/pokemon/' + session.dialogData.pokemon;
+        request(endpoint, function(err, resp, body){
+            if(!err && resp.statusCode == 200){
+                var stats = JSON.parse(body).stats;
+                var value = 0;
+                for (var i = 0; i < stats.length; i++){
+                    if (stats[i].stat.name === data.stat){
+                        value = stats[i].base_stat;
+                    }
+                }
+    			session.send("%s's %s : %s", data.pokemon,
+                    data.stat, value.toString());
+            }
+        });
+        session.endDialog();
+    }
+]).set('storage', inMemoryStorage); //not sure how to use this yet
+
+bot.dialog('askForPokemon',[
+    function(session){
+        builder.Prompts.text(session, 'Which pokemon?');
+    },
+    function(session, results){
+        session.endDialogWithResult(results);
+    }
+]);
+
+bot.dialog('askForStat',[
+    function(session){
+        builder.Prompts.text(session, 'Which stat?');
+    },
+    function(session, results){
+        session.endDialogWithResult(results);
+    }
+]);
