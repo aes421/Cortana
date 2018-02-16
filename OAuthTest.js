b5673bb8661a579b2ed2388d4ae158b90043b935
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-02-16 15:46:34 -0600

    Working on redirecting to reddit

--- OAuthTest.js
+++ OAuthTest.js
@@ -5,21 +5,42 @@ const fs = require('fs');
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-http.createServer(function (req, res){
+const server = http.createServer(function (req, res){
     if (req.url === "/OAuthTest.html"){
-        fs.readFile("OAuthTest.html", function(err, data){
-            if (err){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end(err);
-                return;
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        });
+        serveAuthHTML(req, res);
+    }
+    else if (req.url === "/Authenticate.js?"){
+        redirecting(req, res);
     }
     else{
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("Welcome to my server!")
-        res.end("Request url: " + req.url);
+        serveDefaultHTML(req, res);
     }
 }).listen(8080);
+
+function serveAuthHTML(req, res){
+    fs.readFile("OAuthTest.html", function(err, data){
+        if (err){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end(err);
+            return;
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+}
+
+function serveDefaultHTML(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Welcome to my server!")
+    res.end("Request url: " + req.url);
+}
+
+function redirecting(req, res){
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write("Redirecting");
+    console.log("Redirecting");
+    res.writeHead(301,
+        {Location: 'http://www.google.com'}
+    );
+    res.end();
+}
