f9067dc5d45bb52008f903ce3b910289ddd19049
Ashley Stallings Rhodes <aesdev421@gmail.com>
2018-02-19 20:51:43 -0600

    OAuth implemented - access token returned from reddit

--- api/reddit.js
+++ api/reddit.js
@@ -10,7 +10,7 @@ const CLIENT_SECRET="xEOulPITcRVJww_kfHCjGqRXYZs";
 
 const TYPE="code";
 const RANDOM_STRING="random_string";
-const URI= encodeURIComponent('http://localhost:8080/api/reddit/callback');
+const URI= 'http://localhost:8080/api/reddit/callback';
 const DURATION="temporary";
 const SCOPE_STRING="identity";
 const GRANT_TYPE='authorization_code';
@@ -20,46 +20,13 @@ const GRANT_TYPE='authorization_code';
 const router = express.Router();
 
 router.get('/login', function(req, res){
-    console.log(`Redirecting to: https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=${TYPE}&state=${RANDOM_STRING}&redirect_uri=${URI}&duration=${DURATION}&scope=${SCOPE_STRING}`);
-    res.redirect(`https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=${TYPE}&state=${RANDOM_STRING}&redirect_uri=${URI}&duration=${DURATION}&scope=${SCOPE_STRING}`);
+      res.redirect(`https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=${TYPE}&state=${RANDOM_STRING}&redirect_uri=${URI}&duration=${DURATION}&scope=${SCOPE_STRING}`);
 });
 
 router.get('/callback', function(req, res){
     if (!req.query.code) throw new Error('NoCodeProvided');
     if (req.query.state != RANDOM_STRING) throw new error('Mismatched strings');
     const CODE = req.query.code;
-
-// var postData = {
-//       grant_type: GRANT_TYPE,
-//       code: CODE,
-//       redirect_uri: URI
-//   };
-
-//     var options = {
-//         method: "POST",
-//         hostname: "www.reddit.com",
-//         path: "/api/v1/access_token",
-//         auth: `${CLIENT_ID}:${CLIENT_SECRET}`
-        
-//     };
-
-//     console.log("Starting swap for access token");
-//     var post = http.request(options, callback);
-
-//     post.on('error', (e) => {
-//         console.error(`problem with request: ${e.message}`);
-//       });
-//     console.log("posting Data");
-//     post.write(postData.toString());
-//     post.end();
-// });
-
-// function callback(req, res){
-//     console.log("Req: " + req);
-//     console.log("Res: " + res);
-//     //const json = JSON.parse(res);
-//     //res.redirect(`/?token=${json.access_token}`);
-// };
 
   request('https://www.reddit.com/api/v1/access_token', {
     method: 'POST',
@@ -72,33 +39,16 @@ router.get('/callback', function(req, res){
       username: CLIENT_ID,
       password: CLIENT_SECRET
     }
-  }, function (error, res, body){
+  }, function (error, response, body){
     if (error){
       console.log("Error: " + error);
       return;
     }
-    console.log(res);
-    console.log(res.access_token);
-    console.log(CODE);
-    //const json = res.json();
-    //res.redirect(`/?token=${json.access_token}`);
+    const json = JSON.parse(body);
+    console.log(json.access_token);
+    //res.redirect("http://www.google.com");//(`/?token=${json.access_token}`);
+    res.redirect(`http://localhost:8080/?token=${json.access_token}`);
   });
 });
-
-// router.get('/callback', catchAsync(async (req, res) => {
-//     if (!req.query.code) throw new Error('NoCodeProvided');
-//     const CODE = req.query.code;
-//     const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-//     const response = await fetch(`https://reddit.com/api/v1/access_token?grant_type=${GRANT_TYPE}&code=${CODE}&redirect_uri=${URI}`,
-//       {
-//         method: 'POST',
-//         headers: {
-//           Authorization: `Basic ${creds}`,
-//         },
-//       });
-//     const json = await response.json();
-//     console.log(response.json());
-//     res.redirect(`/?token=${json.access_token}`);
-//   }));
 
 module.exports = router;
